6156b3bafdfba25007e3e35a834470981e93e996
Kshitij Chauhan <kshitijchauhan695@gmail.com>
2026-05-03 18:29:39 +0000

    updated streamlit app

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("🧠 Brain Tumor Analysis")
 
 st.header("Welcome to the Brain Tumour Analysis App")
 
-st.write("Analyze brain MRI scans to generate reports for tumor classification and segmentation, providing both prediction and visual localization in one place.")
+st.write("Analyze brain MRI & CT scans to generate reports for tumor classification and segmentation, providing both prediction and visual localization in one place.")
 
